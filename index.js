6cefff0e83e5673b162adb55d01484c1451632f8
cccaelum <ccc.trad@gmail.com>
2024-09-03 17:07:11 +0200

    'terminado'

---
+++ index.js
@@ -0,0 +1,48 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+const app = express()
+
+const url = 'https://cccaelum.github.io/project-break-dashboard/'
+
+app.get('/', (req, res) => {
+    axios.get(url).then((response) => {
+        if(response.status === 200) {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            const pageTitle = $('title').text()
+
+            const links = [];
+            const imgs = [];
+
+            $('a').each((index, element) => {
+                const link = $(element).attr('href')
+                links.push(link)
+            })
+
+            $('img').each((index, element) => {
+                const img = $(element).attr('src')
+                imgs.push(img)
+            })
+
+            console.log(links)
+            console.log(imgs)
+
+            res.send(`
+                <h1>${pageTitle}</h1>
+                <h2>Enlaces</h2>
+                <ul>
+                    ${links.map(link => `<li><a href="${url}${link}">${link}</a></li>`).join('')}
+                </ul>
+                <h2>Imagenes</h2>
+                <ul>
+                    ${imgs.map(img => `<li><a href="${url}${img}">${img}</a></li>`).join('')}
+                </ul>
+                `)
+        }
+    })
+})
+app.listen(3000, () => {
+    console.log('express esta escuchando en el puerto http://localhost:3000')
+})
